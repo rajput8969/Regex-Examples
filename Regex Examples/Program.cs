7665c7365c7f7865b34173a2e363b8a2caa840dc
ANMOL <anmol999rajput@gmail.com>
2022-03-25 20:24:29 +0530

    Email Validation

--- Regex Examples/Program.cs
+++ Regex Examples/Program.cs
@@ -10,14 +10,14 @@ namespace Regex_Examples
     {
         static void Main(string[] args)
         {
-            Regex r = new Regex(@"^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$");
+            Regex r = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
             //class Regex Repesents an immutable regular expression.  
 
-            string[] str = { "132103", "9767101", "732103", "067101", "8967101" };
+            string[] str = { "anmol@gmail.com", "anm@", "teest@gmail.com", "test@gmail.com" };
             //Input strings for Match valid mobile number.  
             foreach (string s in str)
             {
-                Console.WriteLine("{0} {1} a valid pin-code number.", s,
+                Console.WriteLine("{0} {1} a valid gmail.", s,
                 r.IsMatch(s) ? "is" : "is not");
                 //The IsMatch method is used to validate a string or  
                 //to ensure that a string conforms to a particular pattern.  
