275f3536d3dc69db4e7879ced9e4160f22ff9518
ANMOL <anmol999rajput@gmail.com>
2022-03-25 20:19:21 +0530

    Pin-code Validatio

--- Regex Examples/Program.cs
+++ Regex Examples/Program.cs
@@ -10,14 +10,14 @@ namespace Regex_Examples
     {
         static void Main(string[] args)
         {
-            Regex r = new Regex(@"^\+?\d{0,2}\-?\d{4,5}\-?\d{5,6}");
+            Regex r = new Regex(@"^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$");
             //class Regex Repesents an immutable regular expression.  
 
-            string[] str = { "+91-978967101", "9767101", "+91-9678-967101", "+91-96789-67101", "+919678967101" };
+            string[] str = { "132103", "9767101", "732103", "067101", "8967101" };
             //Input strings for Match valid mobile number.  
             foreach (string s in str)
             {
-                Console.WriteLine("{0} {1} a valid mobile number.", s,
+                Console.WriteLine("{0} {1} a valid pin-code number.", s,
                 r.IsMatch(s) ? "is" : "is not");
                 //The IsMatch method is used to validate a string or  
                 //to ensure that a string conforms to a particular pattern.  
