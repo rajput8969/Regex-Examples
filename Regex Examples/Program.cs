e6893e72cd6cc2e6d13d5a17d05805d1db1f3940
ANMOL SINGH RAJPUT <80009167+rajput8969@users.noreply.github.com>
2022-03-28 18:19:51 +0530

    Update Program.cs

--- Regex Examples/Program.cs
+++ Regex Examples/Program.cs
@@ -10,14 +10,14 @@ namespace Regex_Examples
     {
         static void Main(string[] args)
         {
-            Regex r = new Regex(@"^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$");
+            Regex r = new Regex(@"^[A-Z a-z]{2,25}$");
             //class Regex Repesents an immutable regular expression.  
 
-            string[] str = { "132103", "9767101", "732103", "067101", "8967101" };
+            string[] str = { "ABc","RAm","SHyam" };
             //Input strings for Match valid pin-code number.  
             foreach (string s in str)
             {
-                Console.WriteLine("{0} {1} a valid pin-code number.", s,
+                Console.WriteLine("{0} {1} a valid Name with 2 captial.", s,
                 r.IsMatch(s) ? "is" : "is not");
                 //The IsMatch method is used to validate a string or  
                 //to ensure that a string conforms to a particular pattern.  
